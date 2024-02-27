63882c5325f80d85df989efb17cf4020c9c68c93
Patryk Szewczyk <127959525+Patryk-Szewczyk@users.noreply.github.com>
2024-02-28 00:02:05 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,10 @@
 using System;
 using System.Collections.Generic;
-using Controler;
+using Game;
 using Page_Intro;
 using Page_Menu;
-//using Page_PVP;
-//using Page_PVC;
-using Page_Instructions;
-//using Page_Ranking;
-//using Page_Options;
-//using Page_Credits;
 
-namespace Controler
+namespace Game
 {
     internal class Program
     {
