081bf074a9425e636bd98f16483cc07f82a5dd05
Patryk Szewczyk <127959525+Patryk-Szewczyk@users.noreply.github.com>
2024-02-27 19:13:08 +0100

    Update Page_Intro.cs

--- Page_Intro.cs
+++ Page_Intro.cs
@@ -1,4 +1,4 @@
-﻿namespace Page_Intro
+namespace Page_Intro
 {
     public class IntroPage
     {
@@ -11,14 +11,14 @@
             Console.WriteLine("BB    BB  BB    BB     BB        BB     BB        BB              BB  BB    BB  BB  BB            BB");
             Console.WriteLine("BB    BB  BB    BB     BB        BB     BB        BB              BB  BB    BB  BB  BB           BB");
             Console.WriteLine("BBBBBBB   BB    BB     BB        BB     BBBBBBBB  BBBBBBBB  BBBBBBB   BB    BB  BB  BB          BBBBBBBB");
-            Console.WriteLine("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+            Console.WriteLine("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
             Console.WriteLine("Battleship [Version 2.00]");
             Console.WriteLine("Copyright (c) Patryk Szewczyk. All rights reserved.\n");   // Prawa autorskie zastrzeżone. Tylko do własnego użytku. Zakaz kopiowana i zarabiania na tym produkcie.
             Console.WriteLine("Battleship Game is simple game which depend of sunking ships between players.");
             Console.WriteLine("To start game you must do a few activites to mainly set ships.\n");
             Console.WriteLine("I yery please YOU about enable full screen.");
             Console.WriteLine("In else case you see errors in figure of NOT clearing console code (in console top).");
-            Console.WriteLine("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+            Console.WriteLine("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
             Console.WriteLine("To continue with the instructions, click ENTER key\n");
             Console.ReadLine();   // Aby przejść dalej do kolejnej pseudo-strony konsoli (metoda "Console.Clear()") trzeba kliknąć ENTER
         }
