29585b5818f7f819ed8098b477f46164ab5a946d
Patryk Szewczyk <127959525+Patryk-Szewczyk@users.noreply.github.com>
2024-02-27 19:13:36 +0100

    Update Page_Menu.cs

--- Page_Menu.cs
+++ Page_Menu.cs
@@ -1,18 +1,21 @@
+using Page_Instructions;
+
 namespace Page_Menu
 {
     interface IManuPage
     {
-        public static void Menu() { }
+        public static void Menu() { }   // Wyświetelenie strony menu.
     }
     public class MenuPage : IManuPage
     {
         public static bool isMenuButtonLoop = true;
-        public static string[] menuButtons = { "PVP Mode", "PVC Mode", "Ranking", "Options", "Credits", "Exit" };
-        public static int menuButtNum = 6;
+        public static string[] menuButtons = { "PVP Mode", "PVC Mode", "Instruction", "Ranking", "Options", "Credits", "Exit" };
+        public static int menuButtNum = menuButtons.Length;   // Zawsze ostatni, bo chcę mieć kursor na górze!
         
         public static void Menu()
         {
-            while (isMenuButtonLoop == true)
+
+            while (isMenuButtonLoop == true)   // MENU
             {
                 Console.Clear();
                 Console.WriteLine("BBBBBBB     BBBB    BBBBBBBB  BBBBBBBB  BB        BBBBBBBB   BBBBBBB  BB    BB  BB  BBBBBBB      BBBBBB");
@@ -22,9 +25,9 @@ namespace Page_Menu
                 Console.WriteLine("BB    BB  BB    BB     BB        BB     BB        BB              BB  BB    BB  BB  BB            BB");
                 Console.WriteLine("BB    BB  BB    BB     BB        BB     BB        BB              BB  BB    BB  BB  BB           BB");
                 Console.WriteLine("BBBBBBB   BB    BB     BB        BB     BBBBBBBB  BBBBBBBB  BBBBBBB   BB    BB  BB  BB          BBBBBBBB");
-                Console.WriteLine("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
-                Console.WriteLine("To continue choose some option: (navigation = arrows/wsad, press = ENTER)");
-                for (int i = 0, j = 6; i < menuButtons.Length; i++, j--)
+                Console.WriteLine("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+                Console.WriteLine("MENU: (navigation = arrows/wsad, click = ENTER)\n");
+                for (int i = 0, j = menuButtons.Length; i < menuButtons.Length; i++, j--)
                 {
                     if (j == menuButtNum)
                     {
@@ -35,20 +38,22 @@ namespace Page_Menu
                         Console.WriteLine("  " + menuButtons[i]);
                     }
                 }
+                // ASCII:
                 int asciiValue = 0;
                 ConsoleKeyInfo key = Console.ReadKey(true);
                 if (key.Key != ConsoleKey.Enter)
                 {
                     Console.Write("");
                     asciiValue = (int)key.Key;
-                }
+                } 
                 else
                 {
-                    isMenuButtonLoop = false;   // Wybranie danej opcji.
+                    isMenuButtonLoop = false;
                 }
+                // Poruszanie się po przyciskach (obliczenia):
                 if (asciiValue == 87 || asciiValue == 38)
                 {
-                    menuButtNum = (menuButtNum < 6) ? menuButtNum += 1 : menuButtNum;
+                    menuButtNum = (menuButtNum < menuButtons.Length) ? menuButtNum += 1 : menuButtNum;
                     Console.WriteLine(menuButtNum);
                 }
                 else if (asciiValue == 83 || asciiValue == 40)
@@ -56,6 +61,25 @@ namespace Page_Menu
                     menuButtNum = (menuButtNum > 1) ? menuButtNum -= 1 : menuButtNum;
                     Console.WriteLine(menuButtNum);
                 }
+            }
+            switch (menuButtNum)
+            {
+                case 7:   // PVP
+                    break;
+                case 6:   // PVC
+                    break;
+                case 5:   // Instruction
+                    PageInstructions.Instruction();
+                    break;
+                case 4:   // Ranking
+                    break;
+                case 3:   // Options
+                    break;
+                case 2:   // Credits
+                    break;
+                case 1:   // Exit
+                    isMenuButtonLoop = false;
+                    break;
             }
         }
     }
