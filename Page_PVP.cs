6e89ecac7f9b21f6dd5171336daaa09cbe197d96
Patryk Szewczyk <127959525+Patryk-Szewczyk@users.noreply.github.com>
2024-03-01 22:49:08 +0100

    Update Page_PVP.cs

--- Page_PVP.cs
+++ Page_PVP.cs
@@ -6,7 +6,7 @@ namespace Page_PVP
     // KIEDY SKOŃCZYSZ GRĘ - SPRÓBUJ ZAAPLICOWAĆ "static" DO INTERFEJSÓW STRON!
     interface IPagePVP   // Mogłem opuścić interfejs, aby mieć metody statyczne, ale używam go ponieważ chcę mieć widoczne na górze nazwy wszystkich metody danej klasy:
     {
-        public void PVP();   // Wyświetlenie strony PVP.
+        void PVP();   // Wyświetlenie strony PVP.
     }
     public class PagePVP : IPagePVP
     {
@@ -15,18 +15,23 @@ namespace Page_PVP
         {
             while (isPVPShipPositingLoop == true)
             {
-                Console.Clear();
-                Console.WriteLine("BBBBBBB   BB    BB  BBBBBBB ");
-                Console.WriteLine("BB    BB  BB    BB  BB    BB");
-                Console.WriteLine("BB    BB  BB    BB  BB    BB");
-                Console.WriteLine("BBBBBBB   BB    BB  BBBBBBB ");
-                Console.WriteLine("BB         BB  BB   BB      ");
-                Console.WriteLine("BB          BBBB    BB      ");
-                Console.WriteLine("BB           BB     BB      ");
-                Console.WriteLine("\n- - - - - - - - - - - - - -\n");
-                Console.WriteLine("Back to menu: [Q]\n");
-                ConsoleKeyInfo key = Console.ReadKey(true);
-                if (key.Key == ConsoleKey.Q)
+                MenuPage.menuSoundtrack.Stop();   // Test poprwności zamykania i ponownego odtwierania ścieżki dźwiękowej | ?
+                MenuPage.menuSoundtrack_PLAY = false;
+
+                //
+
+                System.Console.Clear();
+                System.Console.WriteLine("BBBBBBB   BB    BB  BBBBBBB ");
+                System.Console.WriteLine("BB    BB  BB    BB  BB    BB");
+                System.Console.WriteLine("BB    BB  BB    BB  BB    BB");
+                System.Console.WriteLine("BBBBBBB   BB    BB  BBBBBBB ");
+                System.Console.WriteLine("BB         BB  BB   BB      ");
+                System.Console.WriteLine("BB          BBBB    BB      ");
+                System.Console.WriteLine("BB           BB     BB      ");
+                System.Console.WriteLine("\n- - - - - - - - - - - - - -\n");
+                System.Console.WriteLine("Back to menu: [Q]\n");
+                System.ConsoleKeyInfo key = System.Console.ReadKey(true);
+                if (key.Key == System.ConsoleKey.Q)
                 {
                     isPVPShipPositingLoop = false;
                     MenuPage.isMenuButtonLoop = true;
