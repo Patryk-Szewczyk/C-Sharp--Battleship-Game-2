09e775ac3e8707c9dca24cb31490bfdbbf9e125b
Patryk Szewczyk <127959525+Patryk-Szewczyk@users.noreply.github.com>
2024-03-01 22:50:36 +0100

    Update Page_Intro.cs

--- Page_Intro.cs
+++ Page_Intro.cs
@@ -4,23 +4,24 @@ namespace Page_Intro
     {
         public static void Intro()
         {
-            Console.WriteLine("BBBBBBB     BBBB    BBBBBBBB  BBBBBBBB  BB        BBBBBBBB   BBBBBBB  BB    BB  BB  BBBBBBB      BBBBBB");
-            Console.WriteLine("BB    BB   BB  BB      BB        BB     BB        BB        BB        BB    BB  BB  BB    BB    BB    BB");
-            Console.WriteLine("BB    BB  BB    BB     BB        BB     BB        BB        BB        BB    BB  BB  BB    BB         BB");
-            Console.WriteLine("BBBBBBBB  BBBBBBBB     BB        BB     BB        BBBBBBBB   BBBBBB   BBBBBBBB  BB  BBBBBBB        BBB");
-            Console.WriteLine("BB    BB  BB    BB     BB        BB     BB        BB              BB  BB    BB  BB  BB            BB");
-            Console.WriteLine("BB    BB  BB    BB     BB        BB     BB        BB              BB  BB    BB  BB  BB           BB");
-            Console.WriteLine("BBBBBBB   BB    BB     BB        BB     BBBBBBBB  BBBBBBBB  BBBBBBB   BB    BB  BB  BB          BBBBBBBB");
-            Console.WriteLine("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
-            Console.WriteLine("Battleship [Version 2.00]");
-            Console.WriteLine("Copyright (c) Patryk Szewczyk. All rights reserved.\n");   // Prawa autorskie zastrzeżone. Tylko do własnego użytku. Zakaz kopiowana i zarabiania na tym produkcie.
-            Console.WriteLine("Battleship Game is simple game which depend of sunking ships between players.");
-            Console.WriteLine("To start game you must do a few activites to mainly set ships.\n");
-            Console.WriteLine("I yery please YOU about enable full screen.");
-            Console.WriteLine("In else case you see errors in figure of NOT clearing console code (in console top).");
-            Console.WriteLine("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
-            Console.WriteLine("To continue with the instructions, click ENTER key\n");
-            Console.ReadLine();   // Aby przejść dalej do kolejnej pseudo-strony konsoli (metoda "Console.Clear()") trzeba kliknąć ENTER
+            System.Console.WriteLine("BBBBBBB     BBBB    BBBBBBBB  BBBBBBBB  BB        BBBBBBBB   BBBBBBB  BB    BB  BB  BBBBBBB      BBBBBB");
+            System.Console.WriteLine("BB    BB   BB  BB      BB        BB     BB        BB        BB        BB    BB  BB  BB    BB    BB    BB");
+            System.Console.WriteLine("BB    BB  BB    BB     BB        BB     BB        BB        BB        BB    BB  BB  BB    BB         BB");
+            System.Console.WriteLine("BBBBBBBB  BBBBBBBB     BB        BB     BB        BBBBBBBB   BBBBBB   BBBBBBBB  BB  BBBBBBB        BBB");
+            System.Console.WriteLine("BB    BB  BB    BB     BB        BB     BB        BB              BB  BB    BB  BB  BB            BB");
+            System.Console.WriteLine("BB    BB  BB    BB     BB        BB     BB        BB              BB  BB    BB  BB  BB           BB");
+            System.Console.WriteLine("BBBBBBB   BB    BB     BB        BB     BBBBBBBB  BBBBBBBB  BBBBBBB   BB    BB  BB  BB          BBBBBBBB");
+            System.Console.WriteLine("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+            System.Console.WriteLine("Battleship [Version 2.00]");
+            System.Console.WriteLine("Copyright (c) Patryk Szewczyk. All rights reserved.\n");   // Prawa autorskie zastrzeżone. Tylko do własnego użytku. Zakaz kopiowana i zarabiania na tym produkcie.
+            System.Console.WriteLine("Battleship Game is simple game which depend of sunking ships between players.");
+            System.Console.WriteLine("To start game you must do a few activites to mainly set ships.\n");
+            System.Console.WriteLine("I yery please YOU about enable full screen.");
+            System.Console.WriteLine("In else case you see errors in figure of NOT clearing console code (in console top).\n");
+            System.Console.WriteLine("Also I please you about turn on sounds to minimum 20%.");
+            System.Console.WriteLine("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+            System.Console.WriteLine("To continue with the instructions, click ENTER key\n");
+            System.Console.ReadLine();   // Aby przejść dalej do kolejnej pseudo-strony konsoli (metoda "Console.Clear()") trzeba kliknąć ENTER
         }
     }
 }
