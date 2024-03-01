8cef88b82f40d7c208d02f78e8eefd8f8a462d1e
Patryk Szewczyk <127959525+Patryk-Szewczyk@users.noreply.github.com>
2024-03-01 22:49:50 +0100

    Update Page_Options.cs

--- Page_Options.cs
+++ Page_Options.cs
@@ -5,55 +5,72 @@ namespace Page_Options
     // KIEDY SKOŃCZYSZ GRĘ - SPRÓBUJ ZAAPLICOWAĆ "static" DO INTERFEJSÓW STRON!
     interface IPageOptions   // Mogłem opuścić interfejs, aby mieć metody statyczne, ale używam go ponieważ chcę mieć widoczne na górze nazwy wszystkich metody danej klasy:
     {
-        public void Options();   // Wyświetlenie strony rankingu.
+        void Options();   // Wyświetlenie strony rankingu.
     }
     public class PageOptions : IPageOptions
     {
         public static bool isOptionsButtonLoop = true;
-        // EJJJJJJJJJJJ!!! Weź dodaj w pętli FOR po elementach poniższej tablicy odpowiednią informację o włączonych i wyłączonych "checkbox'ach"!
-        public static string[] optionsButtons = { "Music", "Sound effects", "AI voice", "Game Credits Presentation", "Delete ranking data: moving = [O][L], choose = [I], delete = [C]", "Delete Machine Learning data: moving = [O][L], choose = [I), delete = [C]" };
+        public static bool isCorrectSign = false;
+        public static string[] optionsButtons = { "Music:                            ON = [E], OFF = [D]",
+                                                  "Sound effects:                    ON = [E], OFF = [D]",
+                                                  "AI voice:                         ON = [E], OFF = [D]",
+                                                  "AI subtitles:                     ON = [E], OFF = [D]",
+                                                  "Game Credits: website / console:  website = [E], console = [D]",
+                                                  "Delete ranking data:              moving = [O][L], choose = [I], delete = [C]",
+                                                  "Delete Machine Learning data:     moving = [O][L], choose = [I], delete = [C]" };
         public static int optionsButtNum = optionsButtons.Length;
         public void Options()
         {
             while (isOptionsButtonLoop == true)
             {
-                Console.Clear();
-                Console.WriteLine(" BBBBBB   ");
-                Console.WriteLine("BB    BB  ");
-                Console.WriteLine("BB    BB  ");
-                Console.WriteLine("BB    BB  ");
-                Console.WriteLine("BB    BB  ");
-                Console.WriteLine("BB    BB  ");
-                Console.WriteLine(" BBBBBB   ");
-                Console.WriteLine("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
-                Console.WriteLine("Options: (arrows/wsad) | Back to menu: [Q]\n");
+                System.Console.Clear();
+                System.Console.WriteLine(" BBBBBB   BBBBBBB   BBBBBBBB  BB   BBBBBB   BBBB  BB   BBBBBBB");
+                System.Console.WriteLine("BB    BB  BB    BB     BB     BB  BB    BB  BB BB BB  BB      ");
+                System.Console.WriteLine("BB    BB  BB    BB     BB     BB  BB    BB  BB BB BB  BB      ");
+                System.Console.WriteLine("BB    BB  BBBBBBB      BB     BB  BB    BB  BB BB BB   BBBBBB ");
+                System.Console.WriteLine("BB    BB  BB           BB     BB  BB    BB  BB BB BB        BB");
+                System.Console.WriteLine("BB    BB  BB           BB     BB  BB    BB  BB BB BB        BB");
+                System.Console.WriteLine(" BBBBBB   BB           BB     BB   BBBBBB   BB  BBBB  BBBBBBB ");
+                System.Console.WriteLine("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+                System.Console.WriteLine("Options: (arrows/wsad) | Back to menu: [Q]\n");
                 for (int i = 0, j = optionsButtons.Length; i < optionsButtons.Length; i++, j--)
                 {
                     if (j == optionsButtNum)
                     {
-                        Console.WriteLine("> " + optionsButtons[i]);
+                        System.Console.WriteLine("> " + optionsButtons[i]);
                     }
                     else
                     {
-                        Console.WriteLine("  " + optionsButtons[i]);
+                        System.Console.WriteLine("  " + optionsButtons[i]);
                     }
                 }
-                Console.WriteLine("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
-                switch (optionsButtNum)
+                System.Console.WriteLine("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+
+                /*switch (optionsButtNum)
                 {
-                    //
+                    // INFORMACJE O STANIE POSZCZEGÓLNYCH OPCJI, np. Music: OFF
+                }*/
+                System.ConsoleKey key = System.ConsoleKey.Backspace;   // Dowolny niewłaściwy klawisz.
+                while (isCorrectSign == false)   // Pętla ta uniemożliwia przeładowanie strony kiedy kliknie się niewłaściwy klawisz.
+                {
+                    System.ConsoleKeyInfo corr_key = System.Console.ReadKey(true);
+                    if (corr_key.Key == System.ConsoleKey.W || corr_key.Key == System.ConsoleKey.S || corr_key.Key == System.ConsoleKey.UpArrow || corr_key.Key == System.ConsoleKey.DownArrow || corr_key.Key == System.ConsoleKey.Q)
+                    {
+                        isCorrectSign = true;
+                        key = corr_key.Key;
+                    }
                 }
+                isCorrectSign = false;
                 // Poruszanie się po przyciskach (obliczenia):
-                ConsoleKeyInfo key = Console.ReadKey(true);
-                if (key.Key == ConsoleKey.UpArrow || key.Key == ConsoleKey.W)
+                if (key == System.ConsoleKey.UpArrow || key == System.ConsoleKey.W)
                 {
                     optionsButtNum = (optionsButtNum < optionsButtons.Length) ? optionsButtNum += 1 : optionsButtNum;
                 }
-                else if (key.Key == ConsoleKey.DownArrow || key.Key == ConsoleKey.S)
+                else if (key == System.ConsoleKey.DownArrow || key == System.ConsoleKey.S)
                 {
                     optionsButtNum = (optionsButtNum > 1) ? optionsButtNum -= 1 : optionsButtNum;
                 }
-                else if (key.Key == ConsoleKey.Q)
+                else if (key == System.ConsoleKey.Q)
                 {
                     isOptionsButtonLoop = false;
                     MenuPage.isMenuButtonLoop = true;
