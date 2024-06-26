4f88309ce06f93d61a838c5bcdf6f7188ff5dc47
Patryk Szewczyk <127959525+Patryk-Szewczyk@users.noreply.github.com>
2024-06-26 10:24:03 +0200

    Add files via upload

---
+++ GlobalMethods_Bank.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GlobalMethods_Bank
+{
+    public class GlobalMethod
+    {
+        public static void Color(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ResetColor();
+        }
+    }
+}
