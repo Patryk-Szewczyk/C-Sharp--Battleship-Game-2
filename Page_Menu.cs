88a4ea8e8c843a8df126a70621646b19cff28ee2
Patryk Szewczyk <127959525+Patryk-Szewczyk@users.noreply.github.com>
2024-06-28 13:10:39 +0200

    Update Page_Menu.cs

--- Page_Menu.cs
+++ Page_Menu.cs
@@ -1,149 +1,218 @@
-// Te przestrzenie nazw są tu POTRZEBNE, ponieważ metoda MENU odwołuje się do klas tych przestrzeni nazw.
 using System;
-using System.Media;
-using Page_Instructions;
-using Page_Credits;
-using Page_PVP;
-using Page_PVC;
-using Page_Ranking;
-using Page_Options;
+using System.IO;
+using System.Linq.Expressions;
+using Page_Menu;
 
-namespace Page_Menu
+namespace Page_PVC
 {
-    public class MenuPage
-    {
-        public static SoundPlayer currentSoundtrack = new SoundPlayer();   // WEŹ TO SPRÓBUJ PRZENIEŚĆ DO MENU Z try/catch
-        public static bool menuSoundtrack_PLAY = false;
-        public static bool creditsSoundtrack_PLAY = false;
-        public static bool isMenuButtonLoop = true;
-        public static bool isCorrectSign = false;
-        public static string[] menuButtons = { "PVC Mode", "Instruction", "Ranking", "Options", "Credits", "Exit" };
-        public static int menuButtNum = menuButtons.Length;   // Zawsze ostatni, bo chcę mieć kursor na górze!
 
-        public static void Menu()
+    // KIEDY SKOŃCZYSZ GRĘ - SPRÓBUJ ZAAPLICOWAĆ "static" DO INTERFEJSÓW STRON!
+    /*interface IPagePVC   // Mogłem opuścić interfejs, aby mieć metody statyczne, ale używam go ponieważ chcę mieć widoczne na górze nazwy wszystkich metody danej klasy:
+    {
+        void PVC();   // Wyświetlenie strony PVC.
+        void SelectPlayer();   // Etap 1: Wybór gracza i tworzenie gracza.
+        void SetShips_PLAYER();   // Etap 2: Ustawienie statków gracza.
+        void SetShips_COMPUTER();   // Etap 3: Ustawienie statków dla komputera.
+        void BattleControl();   // Etap 4: Bitwa (panel kontrolny).
+        void Battle_PLAYER();   // Przekierowanie na gracza.
+        void Battle_COMPUTER();   // Przekierowanie na komputer.
+
+        void SubmitRanking();   // Etap 5: Wpisz wynik do rankingu i pokaż ranking.
+    }*/
+    public class PagePVC/* : IPagePVC*/
+    {
+        public static bool isPVCShipPositingLoop = true;
+        public static System.ConsoleKeyInfo mainKey;
+        public static int playersLimit = 20;
+        public static int playerButtNum = 0;   // Zawsze ostatni, bo chcę mieć kursor na górze!
+        public void PVC()
         {
-            System.ConsoleKey key = System.ConsoleKey.Backspace;   // Dowolny niewłaściwy klawisz.
-            System.ConsoleKeyInfo corr_key;
-            if (MenuPage.menuSoundtrack_PLAY == false && MenuPage.creditsSoundtrack_PLAY == false)
+            string[,] playersDetails_PARTS = DownloadPlayers();
+            int playerButtNum = playersDetails_PARTS.GetLength(0);
+
+            System.ConsoleKeyInfo key;
+            while (isPVCShipPositingLoop == true)
             {
-                MenuPage.Soundtrack("Soundtracks/Menu/473915__xhale303__synthwave-loop.wav");
-                MenuPage.menuSoundtrack_PLAY = true;
-            }
-            else if (MenuPage.menuSoundtrack_PLAY == false && MenuPage.creditsSoundtrack_PLAY == true)
-            {
-                MenuPage.currentSoundtrack.Stop();
-                MenuPage.creditsSoundtrack_PLAY = false;
-                MenuPage.Soundtrack("Soundtracks/Menu/473915__xhale303__synthwave-loop.wav");
-                MenuPage.menuSoundtrack_PLAY = true;
-            }
-            while (isMenuButtonLoop == true)   // MENU
-            {
+                //MenuPage.currentSoundtrack.Stop();   // Test poprwności zamykania i ponownego odtwierania ścieżki dźwiękowej | OK
+                //MenuPage.menuSoundtrack_PLAY = false;
+
                 Console.Clear();
-                Console.WriteLine("BBBBBBB     BBBB    BBBBBBBB  BBBBBBBB  BB        BBBBBBBB   BBBBBBB  BB    BB  BB  BBBBBBB      BBBBBB");
-                Console.WriteLine("BB    BB   BB  BB      BB        BB     BB        BB        BB        BB    BB  BB  BB    BB    BB    BB");
-                Console.WriteLine("BB    BB  BB    BB     BB        BB     BB        BB        BB        BB    BB  BB  BB    BB         BB");
-                Console.WriteLine("BBBBBBBB  BBBBBBBB     BB        BB     BB        BBBBBBBB   BBBBBB   BBBBBBBB  BB  BBBBBBB        BBB");
-                Console.WriteLine("BB    BB  BB    BB     BB        BB     BB        BB              BB  BB    BB  BB  BB            BB");
-                Console.WriteLine("BB    BB  BB    BB     BB        BB     BB        BB              BB  BB    BB  BB  BB           BB");
-                Console.WriteLine("BBBBBBB   BB    BB     BB        BB     BBBBBBBB  BBBBBBBB  BBBBBBB   BB    BB  BB  BB          BBBBBBBB");
-                Console.WriteLine("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
-                Console.WriteLine("MENU: | Moving: arrows/[W][S] | Click = ENTER/[E]\n");
-                for (int i = 0, j = menuButtons.Length; i < menuButtons.Length; i++, j--)
+                Console.WriteLine("BBBBBBB   BB    BB   BBBBBBB");
+                Console.WriteLine("BB    BB  BB    BB  BB      ");
+                Console.WriteLine("BB    BB  BB    BB  BB      ");
+                Console.WriteLine("BBBBBBB   BB    BB  BB      ");
+                Console.WriteLine("BB         BB  BB   BB      ");
+                Console.WriteLine("BB          BBBB    BB      ");
+                Console.WriteLine("BB           BB      BBBBBBB");
+                Console.WriteLine("\n- - - - - - - - - - - - - -\n");
+                Console.WriteLine("PVC MODE: | Moving: arrows/[W][S] | Click = ENTER/[E] | Create player: [C] | Delete player: [P] | Back to menu: [Q] \n");
+
+                Console.WriteLine("Select player:");
+                for (int i = 0, j = playersDetails_PARTS.GetLength(0); i < playersDetails_PARTS.GetLength(0); i++, j--)
                 {
-                    if (j == menuButtNum)
+                    if (j == playerButtNum)
                     {
-                        Console.WriteLine("> " + menuButtons[i]);
+                        Console.WriteLine("> " + playersDetails_PARTS[i, 0]);
                     }
                     else
                     {
-                        Console.WriteLine("  " + menuButtons[i]);
+                        Console.WriteLine("  " + playersDetails_PARTS[i, 0]);
                     }
                 }
-                while (isCorrectSign == false)   // Pętla ta uniemożliwia przeładowanie strony kiedy kliknie się niewłaściwy klawisz.
+
+                //PagePVC.SetShips_PLAYER(PagePVC.mainKey);   // Ta medota musi być przrd metodą "Console.ReadKey()", ponieważ jej wynik musi być
+                // wyswietlony po metodzie "Console.Clear()", której stan (stan wyświetlacza konsoli) jest zatrzymywany przez "Console.ReadKey()".
+
+                PagePVC.mainKey = Console.ReadKey(true);
+                if (PagePVC.mainKey.Key == System.ConsoleKey.Q)
                 {
-                    corr_key = Console.ReadKey(true);
-                    if (corr_key.Key == System.ConsoleKey.W || corr_key.Key == System.ConsoleKey.S || corr_key.Key == System.ConsoleKey.UpArrow || corr_key.Key == System.ConsoleKey.DownArrow || corr_key.Key == System.ConsoleKey.E || corr_key.Key == System.ConsoleKey.Enter)
-                    {
-                        isCorrectSign = true;
-                        key = corr_key.Key;
-                    }
-                }
-                isCorrectSign = false;
-                if (key == System.ConsoleKey.Enter || key == System.ConsoleKey.E)   // ENTER / [E]
-                {
-                    isMenuButtonLoop = false;
-                    switch (menuButtNum)
-                    {
-                        /*case 7:   // PVP
-                            PagePVP.isPVPShipPositingLoop = true;
-                            PagePVP pvp = new PagePVP();
-                            pvp.PVP();
-                            //GC.Collect();   // Usuwanie obiektu z pamięci, aby nie zaśmiecać jej dodatkowymi kopiami tego opiektu. (wiem, że on sam się włącza, a chcę mieć 100% pewność, że jednak obiekt ten znika z pamięci)
-                            //GC.WaitForPendingFinalizers();
-                            break;*/
-                        case 6:   // PVC
-                            PagePVC.isPVCShipPositingLoop = true;
-                            PagePVC pvc = new PagePVC();
-                            pvc.PVC();
-                            //GC.Collect();
-                            //GC.WaitForPendingFinalizers();
-                            break;
-                        case 5:   // Instruction
-                            PageInstructions.isInstructionButtonLoop = true;
-                            PageInstructions instruction = new PageInstructions();
-                            instruction.Instructions();
-                            //GC.Collect();
-                            //GC.WaitForPendingFinalizers();
-                            break;
-                        case 4:   // Ranking
-                            PageRanking.isRankingButtonLoop = true;
-                            PageRanking ranking = new PageRanking();
-                            ranking.Ranking();
-                            //GC.Collect();
-                            //GC.WaitForPendingFinalizers();
-                            break;
-                        case 3:   // Options
-                            PageOptions.isOptionsButtonLoop = true;
-                            PageOptions options = new PageOptions();
-                            options.Options();
-                            //GC.Collect();
-                            //GC.WaitForPendingFinalizers();
-                            break;
-                        case 2:   // Credits
-                            PageCredits.isCreditsLoop = true;
-                            PageCredits credits = new PageCredits();
-                            credits.Credits();
-                            //GC.Collect();
-                            //GC.WaitForPendingFinalizers();
-                            break;
-                        case 1:   // Exit
-                            isMenuButtonLoop = false;
-                            break;
-                    }
+                    isPVCShipPositingLoop = false;
+                    MenuPage.isMenuButtonLoop = true;
+                    MenuPage.Menu();
                 }
                 // Poruszanie się po przyciskach (obliczenia):
-                if (key == System.ConsoleKey.UpArrow || key == System.ConsoleKey.W)
+                if (PagePVC.mainKey.Key == System.ConsoleKey.UpArrow || PagePVC.mainKey.Key == System.ConsoleKey.W)
                 {
-                    menuButtNum = (menuButtNum < menuButtons.Length) ? menuButtNum += 1 : menuButtNum;
+                    playerButtNum = (playerButtNum < playersDetails_PARTS.GetLength(0)) ? playerButtNum += 1 : playerButtNum;
                 }
-                else if (key == System.ConsoleKey.DownArrow || key == System.ConsoleKey.S)
+                else if (PagePVC.mainKey.Key == System.ConsoleKey.DownArrow || PagePVC.mainKey.Key == System.ConsoleKey.S)
                 {
-                    menuButtNum = (menuButtNum > 1) ? menuButtNum -= 1 : menuButtNum;
+                    playerButtNum = (playerButtNum > 1) ? playerButtNum -= 1 : playerButtNum;
                 }
             }
         }
-        public static void Soundtrack(string filepath)
+        public static string[,] DownloadPlayers()
         {
-            try   // W przypadku braku ścieżki dżwiękowej, wyjątek zostanie złapany, przez co program nie zakończy działania.
+            string filePath = "players.txt";
+            string content = "Gracz 1";
+
+            /*try
             {
-                MenuPage.currentSoundtrack.SoundLocation = filepath;
-                MenuPage.currentSoundtrack.PlayLooping();
-                //Console.WriteLine("STAN MUZYKI: Znaleziono\n\n" + filepath);
-            } catch { }
-            /*catch (Exception error)
+                // Zapisz tekst do pliku
+                File.WriteAllText(filePath, content);
+                Console.WriteLine("Data has been written to the file.");
+            }
+            catch (IOException ex)
             {
-                //Console.WriteLine("STAN MUZYKI: Nie znaleziono\n\n" + error);
+                Console.WriteLine("An error occurred while writing to the file:");
+                Console.WriteLine(ex.Message);
             }*/
+
+            
+            // Odczytaj cały tekst z pliku
+            string fileContent = File.ReadAllText(filePath);
+            string[] players = fileContent.Split('*');
+            string[,] playersDetails_PARTS = new string[players.Length, 5];
+            string[] playerDetails_BLOCK = null;
+            for (int i = 0; i < players.Length; i++)
+            {
+                // Każdy gracz ma 5 informacji oddzielonych znakiem "#":
+                playerDetails_BLOCK = players[i].Split('#');
+                for (int j = 0; j < playerDetails_BLOCK.Length; j++)
+                {
+                    playersDetails_PARTS[i, j] = playerDetails_BLOCK[j];
+                }
+            }
+            return playersDetails_PARTS;
+        }
+        public static void SetShips_PLAYER(System.ConsoleKeyInfo key)
+        {
+            // Walidacja poruszania się po planszy:
+            int setVal = SetShips_PLAYER_CLASS.CursorNavigate(key);
+            Console.WriteLine("Set value: " + setVal);
+        }
+        public class SetShips_PLAYER_CLASS
+        {
+            public static int cursorVal = 0;
+            public static int[] toUpBlock_AR = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            public static int[] toDownBlock_AR = new int[10] { 90, 91, 92, 93, 94, 95, 96, 97, 98, 99 };
+            public static int[] toRightBlock_AR = new int[10] { 9, 19, 29, 39, 49, 59, 69, 79, 89, 99 };
+            public static int[] toLeftBlock_AR = new int[10] { 0, 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+            public static bool toUpBlock_BOOL = false, toDownBlock_BOOL = false, toRightBlock_BOOL = false, toLeftBlock_BOOL = false;
+            public static int CursorNavigate(System.ConsoleKeyInfo key)
+            {
+                SetShips_PLAYER_CLASS.toUpBlock_BOOL = false;
+                SetShips_PLAYER_CLASS.toDownBlock_BOOL = false;
+                SetShips_PLAYER_CLASS.toRightBlock_BOOL = false;
+                SetShips_PLAYER_CLASS.toLeftBlock_BOOL = false;
+                for (int i = 0; i < SetShips_PLAYER_CLASS.toUpBlock_AR.Length; i++)
+                {
+                    if (SetShips_PLAYER_CLASS.cursorVal == toUpBlock_AR[i])
+                    {
+                        SetShips_PLAYER_CLASS.toUpBlock_BOOL = true;
+                    }
+                }
+                if (SetShips_PLAYER_CLASS.toUpBlock_BOOL == false)
+                {
+                    if (key.Key == System.ConsoleKey.UpArrow || key.Key == System.ConsoleKey.W)
+                    {
+                        cursorVal -= 10;
+                    }
+                }
+                for (int i = 0; i < SetShips_PLAYER_CLASS.toDownBlock_AR.Length; i++)
+                {
+                    if (SetShips_PLAYER_CLASS.cursorVal == toDownBlock_AR[i])
+                    {
+                        SetShips_PLAYER_CLASS.toDownBlock_BOOL = true;
+                    }
+                }
+                if (SetShips_PLAYER_CLASS.toDownBlock_BOOL == false)
+                {
+                    if (key.Key == System.ConsoleKey.DownArrow || key.Key == System.ConsoleKey.S)
+                    {
+                        cursorVal += 10;
+                    }
+                }
+                for (int i = 0; i < SetShips_PLAYER_CLASS.toRightBlock_AR.Length; i++)
+                {
+                    if (SetShips_PLAYER_CLASS.cursorVal == toRightBlock_AR[i])
+                    {
+                        SetShips_PLAYER_CLASS.toRightBlock_BOOL = true;
+                    }
+                }
+                if (SetShips_PLAYER_CLASS.toRightBlock_BOOL == false)
+                {
+                    if (key.Key == System.ConsoleKey.RightArrow || key.Key == System.ConsoleKey.D)
+                    {
+                        cursorVal += 1;
+                    }
+                }
+                for (int i = 0; i < SetShips_PLAYER_CLASS.toLeftBlock_AR.Length; i++)
+                {
+                    if (SetShips_PLAYER_CLASS.cursorVal == toLeftBlock_AR[i])
+                    {
+                        SetShips_PLAYER_CLASS.toLeftBlock_BOOL = true;
+                    }
+                }
+                if (SetShips_PLAYER_CLASS.toLeftBlock_BOOL == false)
+                {
+                    if (key.Key == System.ConsoleKey.LeftArrow || key.Key == System.ConsoleKey.A)
+                    {
+                        cursorVal -= 1;
+                    }
+                }
+                return SetShips_PLAYER_CLASS.cursorVal;
+            }
+        }
+        public void SetShips_COMPUTER()
+        {
+
+        }
+        public void BattleControl()
+        {
+
+        }
+        public void Battle_PLAYER()
+        {
+
+        }
+        public void Battle_COMPUTER()
+        {
+
+        }
+        public void SubmitRanking()
+        {
+
         }
     }
 }
