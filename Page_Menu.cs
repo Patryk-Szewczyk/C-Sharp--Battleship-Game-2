a187a6c0c0509e9e9d34bb28199c4c2dcfc531ae
Patryk Szewczyk <127959525+Patryk-Szewczyk@users.noreply.github.com>
2024-09-21 12:26:53 +0200

    Update Page_Menu.cs

--- Page_Menu.cs
+++ Page_Menu.cs
@@ -8,6 +8,7 @@ using Page_PVC;
 using Page_Ranking;
 using Page_Options;
 using Library_GlobalMethods;
+using System.Diagnostics.Eventing.Reader;
 
 namespace Page_Menu {
     public class Menu {
@@ -19,7 +20,10 @@ namespace Page_Menu {
         public static bool isPage = true;
         public static string[] buttons = { "PVC Mode", "Instruction", "Ranking", "Options", "Credits", "Exit" };
         public static int currentButton = 0;   // Zawsze pierwszy, bo chcę mieć kursor na górze!
-        public static List<ConsoleKey> usingKeys = new List<ConsoleKey> { ConsoleKey.W, ConsoleKey.S, ConsoleKey.UpArrow, ConsoleKey.DownArrow, ConsoleKey.Enter };
+        public static List<ConsoleKey> usingKeys_STANDARD = new List<ConsoleKey> { ConsoleKey.W, ConsoleKey.S, ConsoleKey.UpArrow, ConsoleKey.DownArrow, ConsoleKey.Enter };
+        public static List<ConsoleKey> usingKeys_TOP = new List<ConsoleKey> { ConsoleKey.S, ConsoleKey.DownArrow, ConsoleKey.Enter };
+        public static List<ConsoleKey> usingKeys_DOWN = new List<ConsoleKey> { ConsoleKey.W, ConsoleKey.UpArrow, ConsoleKey.Enter };
+        public static List<ConsoleKey> usingKeys_ONE = new List<ConsoleKey> { ConsoleKey.Enter };
         public static Tuple<PVC, Instructions, Ranking, Options, Credits> pages = new Tuple<PVC, Instructions, Ranking, Options, Credits>(
                 new PVC(),
                 new Instructions(),
@@ -34,9 +38,9 @@ namespace Page_Menu {
                 Console.Clear();
                 RenderTitle();
                 GlobalMethod.Page.RenderButtons(buttons, currentButton);
-                key = GlobalMethod.Page.LoopCorrectKey(page_ID, key, usingKeys);   // Pętla ta uniemożliwia przeładowanie strony kiedy kliknie się niewłaściwy klawisz.
+                key = GlobalMethod.Page.SelectUsingKeys(currentButton, page_ID, key, buttons, usingKeys_STANDARD, usingKeys_TOP, usingKeys_DOWN, usingKeys_ONE);   // Pętla ta uniemożliwia przeładowanie strony kiedy kliknie się niewłaściwy klawisz.
                 RenderPage(key, pages);
-                currentButton = GlobalMethod.Page.MoveButtons(buttons, currentButton, key);   // Poruszanie się po przyciskach (obliczenia):
+                currentButton = GlobalMethod.Page.MoveButtons(buttons, currentButton, key);   // Poruszanie się po przyciskach (obliczenia)
             }
             Environment.Exit(0);   // Użyłem tej metody, ponieważ po zapisie danych w klasie "Options" pojawia się problem z wyjściem z pragramu. Strasznie długo się zamyka.
         }
