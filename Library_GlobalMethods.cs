013d0594f3b7c97a919438cb5481c84bca3213c7
Patryk Szewczyk <127959525+Patryk-Szewczyk@users.noreply.github.com>
2024-10-02 22:49:17 +0200

    Update Library_GlobalMethods.cs

--- Library_GlobalMethods.cs
+++ Library_GlobalMethods.cs
@@ -45,16 +45,6 @@ namespace Library_GlobalMethods {
             Console.Write(text);
             Console.ResetColor();
         }
-        public static int SearchRemoveAt(List<int> array, int target) {   // Szukanie wartości i jej indeksu (lokalizacji) w tablicy:
-            int result = -1;   // W kontekście losowania statków dla komputera, oznacza to kolizję pola począttkowego nowego statku z już istniejącym.
-            for (int i = 0; i < array.Count; i++) {
-                if (target == array[i]) {
-                    result = i;
-                    break;
-                }
-            }
-            return result;
-        }
         public static string TrimAllContent(string content) {
             string text = "";
             for (int i = 0; i < content.Length; i++) {
@@ -75,6 +65,46 @@ namespace Library_GlobalMethods {
                 if (i < content.Count - 1) text += "*";
             }
             return text;
+        }
+        public static int SearchRemoveAt(List<int> array, int target) {   // Szukanie wartości i jej indeksu (lokalizacji) w tablicy:
+            int result = -1;   // W kontekście losowania statków dla komputera, oznacza to kolizję pola począttkowego nowego statku z już istniejącym.
+            for (int i = 0; i < array.Count; i++) {
+                if (target == array[i]) {
+                    result = i;
+                    break;
+                }
+            }
+            return result;
+        }
+        public static string ConvertTo_A0(int coordinate) {
+            string corrForm = Convert.ToString(coordinate);
+            switch (corrForm[0]) {
+                case '0': corrForm = "A" + corrForm[0]; break;
+                case '1': corrForm = "A" + corrForm[0]; break;
+                case '2': corrForm = "A" + corrForm[0]; break;
+                case '3': corrForm = "A" + corrForm[0]; break;
+                case '4': corrForm = "A" + corrForm[0]; break;
+                case '5': corrForm = "A" + corrForm[0]; break;
+                case '6': corrForm = "A" + corrForm[0]; break;
+                case '7': corrForm = "A" + corrForm[0]; break;
+                case '8': corrForm = "A" + corrForm[0]; break;
+                case '9': corrForm = "A" + corrForm[0]; break;
+            }
+            if (coordinate > 9) {   // "34"   [0] = 3 | [1] = 4
+                corrForm = Convert.ToString(coordinate);
+                switch (corrForm[0]) {
+                    case '1': corrForm = "B" + corrForm[1]; break;
+                    case '2': corrForm = "C" + corrForm[1]; break;
+                    case '3': corrForm = "D" + corrForm[1]; break;
+                    case '4': corrForm = "E" + corrForm[1]; break;
+                    case '5': corrForm = "F" + corrForm[1]; break;
+                    case '6': corrForm = "G" + corrForm[1]; break;
+                    case '7': corrForm = "H" + corrForm[1]; break;
+                    case '8': corrForm = "I" + corrForm[1]; break;
+                    case '9': corrForm = "J" + corrForm[1]; break;
+                }
+            }
+            return corrForm;
         }
         public class Page {
             public static void RenderDottedLine(int length) {
@@ -181,89 +211,6 @@ namespace Library_GlobalMethods {
                 Sign(sign, isCursor);
                 Console.WriteLine("}");
                 isCursor = false;
-            }
-            public static void Render(List<int> board, string mode, string[,] data) {   // 3 parametr jest "jako wzór, zastanów się jak to ogarnąć..."
-                // Algorytm wyświetlający planszę dla instrukcji, ustawiania statków gracza i bitwy.
-            }
-
-
-
-
-
-            public static List<List<int>> PrepareShips(List<int> shipsInfo) {
-                List<List<int>> shipsList = new List<List<int>>();
-                for (int i = 0; i < shipsInfo.Count; i++) {
-                    List<int> ship = new List<int>();
-                    shipsList.Add(ship);
-                    for (int j = 0; j < shipsInfo[i]; j++) {
-                        shipsList[i].Add(i + 1);
-                    }
-                }
-                return shipsList;
-            }
-            public static List<List<int>> RandomShips(List<int> array, List<List<int>> shipsList) {
-                bool isCor = false;
-                List<int> board = array;
-                string dirVal = "";
-                string[] dirAr = new string[2] { "toRight", "toBottom" };
-                Random rand = new Random();
-                int init = 0;
-                int rem = 0;
-                int shipDist = 0, limit = 0;
-                double minBott = shipsList.Count / 2, bottCount = 0;
-                minBott = (shipsList.Count % 2 == 1) ? minBott = Math.Floor(minBott) + rand.Next(0, 2) : minBott = shipsList.Count / 2;   // Jeżeli mam liczbę nieparzystą, to czy będzie więcej czy mniej statków "toBottom" o 1 zależy od losowości.
-                bool isShip = false;
-                while (!isCor) {
-                    board = new List<int>(array);   // Dlaczego tak, a nie board = array ? Gdyż lista jest przekazywana nie kopią a referencją, w związku z czym odwołuję się do pierwotnie zadeklarowanej listy i zmniejszam ją w nieskończoność, zamiast tworzyć nową kopię.
-                    bottCount = 0;
-                    for (int i = 0; i < shipsList.Count; i++) {
-                        init = rand.Next(0, board.Count);
-                        rem = GlobalMethod.SearchRemoveAt(board, init);
-                        if (rem == -1) break;   // Kolizję pola początkowego nowego statku z już istniejącym.
-                        board.RemoveAt(rem);
-                        dirVal = dirAr[rand.Next(0, dirAr.Length)];
-                        if (dirVal == "toRight") {
-                            shipDist = init + (shipsList[i].Count - 1);   // Obliczanie długości statku na planszy.
-                            limit = (Convert.ToString(init).Length == 1) ? 9 : 9 + (10 * (int)char.GetNumericValue(Convert.ToChar(Convert.ToString(init)[0])));   // Jeżeli statek jest większy niż 1 pole długości - współrzędna inicjacyjna jest "ciachana" w celu dodania odpowiedniej jej częsci do bazowej liczby limitu, aby ostatecznie wyznaczyć odpowiedni limit dla statku znajdującym się w odpowiednim polu. Np. Dla statku o długości 3, w kierunku "toRight" bazawa wartość limitu wynosi 9, jeżeli współrzędna początkowa wynosi 54, to wycinana jest 5, mnożona przez 10 i dodawana do 9, w związku z czym mamy 59. Analogicznie jest w przypadku toBottom, tylko wartości są tak dostosowane aby dotyczyły NIE częsści dziesiętnej, a części jedności. Wówczas będziemy mięli 94.
-                            if (shipDist > limit) break;   // Jeżeli statek wychodzi poza planszę, losuj statki od nowa. Jeżeli np. mamy statek długości 3, "toRight", a współrzędną początkową 54, to limit wynosi 59, a "shipDist" wynosi [współrzędna początkowo] + [długość statku] - 1, czyli 56. Wówczas mamy 56 <= 59, co jest prawdą, więc tworzenie staatku przechodzi ten etap.
-                            if (shipsList[i].Count > 1) {   // Tworzenie pól długości dla statków powyżej 1 pola długości (2, 3, 4 ...).
-                                for (int j = 1; j < shipsList[i].Count; j++) {
-                                    isShip = false;
-                                    rem = GlobalMethod.SearchRemoveAt(board, init + j);
-                                    if (rem != -1) {   // Jeżeli nie ma kolizji statku
-                                        board.RemoveAt(rem);
-                                        shipsList[i][j] = init + j;
-                                        isShip = true;
-                                    }
-                                    if (!isShip) break;   // Dlaczego dwa "break" i warunek? Normalnie wystarczyłby ten, ale ponieważ "break" ogranicza się do najbliższego "for", więc zrobiłem specjalny warunek, za pomocą którego będziemy kontrolować "break" pętli for wewnętrznej i "właściwej" nadrzędnej.
-                                }
-                                if (!isShip) break;
-                            }
-                            shipsList[i][0] = init;   // Jest to na końcu aby nie wciskać wartości gdy reszta pól długości statku będzie miała nieprawidłowe współrzędne. Operacja ta zaoszczędza mocy obliczeniowej.
-                        } else if (dirVal == "toBottom") {
-                            bottCount++;
-                            shipDist = (init * 10) + ((shipsList[i].Count * 10) - 10);
-                            limit = (Convert.ToString(init).Length == 1) ? 90 : 90 + (1 * (int)char.GetNumericValue(Convert.ToChar(Convert.ToString(init)[1])));
-                            if (shipDist > limit) break;
-                            if (shipsList[i].Count > 1) {
-                                for (int j = 10; j < shipsList[i].Count * 10; j=j+10) {
-                                    isShip = false;
-                                    rem = GlobalMethod.SearchRemoveAt(board, init + j);
-                                    if (rem != -1) {
-                                        board.RemoveAt(rem);
-                                        shipsList[i][j/10] = init + j;
-                                        isShip = true;
-                                    }
-                                    if (!isShip) break;
-                                }
-                                if (!isShip) break;
-                            }
-                            shipsList[i][0] = init;
-                        }
-                        if (i == shipsList.Count - 1) if (bottCount == minBott) isCor = true;   // Jeżeli 7 statków uzyskało poprawne wspoółrzędne, przejdź dalej -> Jeżeli liczba statków "toBottom" jest właściwa, zakończ algorytm.
-                    }
-                }
-                return shipsList;
             }
         }
     }
